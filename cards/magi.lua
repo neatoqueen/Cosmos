3ffc2253c2b377c401a30b1491b6e3ba1ea2d933
Lykrast <aymericpm@gmail.com>
2025-02-03 12:08:19 +0100

    Fix more eternal compat (magi/santa)

--- cards/magi.lua
+++ cards/magi.lua
@@ -11,7 +11,7 @@ SMODS.Joker {
     unlocked = true,
     discovered = true,
     blueprint_compat = true,
-    eternal_compat = false,
+    eternal_compat = true,
     perishable_compat = true,
     rarity = 2,
     atlas = "JJPack",
