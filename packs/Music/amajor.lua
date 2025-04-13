2a8b5332bcc32f9c8721f05105cc1bcfb98ac94b
Amy Hurrell <taliajhurrell@gmail.com>
2025-04-13 14:59:23 +0100

    A Major
    Every played Ace, 3 and 5
    permanently gains
    +1 Mult when scored

--- packs/Music/amajor.lua
+++ packs/Music/amajor.lua
@@ -20,5 +20,15 @@ SMODS.Joker {
         }
     end,
     calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play and context.other_card then
+            if context.other_card:get_id() == 14 or context.other_card:get_id() == 3 or context.other_card:get_id() == 5 then
+                context.other_card.ability.perma_mult = (context.other_card.ability.perma_mult or 0) + card.ability.extra.mult_mod
+                return {
+                    extra = { message = localize('k_upgrade_ex'), colour = G.C.MULT },
+                    colour = G.C.MULT,
+                    card = card
+                }
+            end
+        end
     end
 }
