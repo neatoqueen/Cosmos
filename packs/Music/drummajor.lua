4b8895e00b7b65172c1dbc3a98e1b54ea86fe6d5
Amy Hurrell <taliajhurrell@gmail.com>
2025-04-18 17:05:36 +0100

    aces arent numbered

--- packs/Music/drummajor.lua
+++ packs/Music/drummajor.lua
@@ -9,14 +9,14 @@ SMODS.Joker {
     atlas = "MusicAtlas",
     pos = { x = 2, y = 2 },
     cost = 5,
-    config = { extra = { x_mult_mod = 0.05, numbered_cards = 40 } },
+    config = { extra = { x_mult_mod = 0.05, numbered_cards = 36 } },
     in_pool = function(self, args)
         local check = G.cosmos.enabled.Music or false
         return check
     end,
     loc_vars = function(self, info_queue, card)
         return {
-            vars = { card.ability.extra.x_mult_mod, 1 + card.ability.extra.x_mult_mod*(card.ability.extra.numbered_cards or 40) }
+            vars = { card.ability.extra.x_mult_mod, 1 + card.ability.extra.x_mult_mod*(card.ability.extra.numbered_cards or 36) }
         }
     end,
     calculate = function(self, card, context)
@@ -39,7 +39,7 @@ SMODS.Joker {
         if G.deck then
             local c = 0
             for k, v in ipairs(G.deck.cards) do
-                if not v:is_face() then
+                if (not v:is_face()) and v:get_id() ~= 14 then
                     c = c + 1
                 end
             end
